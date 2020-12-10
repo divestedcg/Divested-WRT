aa31c71e2da321b6b707cc9d77582d6d9b3f5df6
Tad <tad@spotco.us>
2020-12-09 23:44:31 -0500

    20201209-00

--- sign.sh
+++ sign.sh
@@ -1,7 +1,18 @@
-for checksum in */sha256sums; do
+#!/bin/bash
+#Copyright (c) 2020 Divested Computing Group
+#License: GPL-2.0
+
+function sign() {
+	checksum=$1;
 	echo -e "\e[0;32mGPG signing $checksum\e[0m";
 	gpg --sign --local-user 6395FC9911EDCD6158712DF7BADFCABDDBF5B694 --clearsign "$checksum";
 	if [ "$?" -eq "0" ]; then
 		mv -f "$checksum.asc" "$checksum";
 	fi;
-done;
+}
+
+function signAll() {
+	for checksum in */sha256sums; do
+		sign "$checksum";
+	done;
+}
